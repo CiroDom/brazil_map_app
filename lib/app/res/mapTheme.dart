06a3954f0bce688867f2187e72aafaa01bc96812
Ciro de Oliveira <c.dom.oliveira@gmail.com>
2023-05-02 17:10:31 -0300

    adição do uso de material3

--- lib/app/res/mapTheme.dart
+++ lib/app/res/mapTheme.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 
 class mapTheme {
   static ThemeData get theme => ThemeData(
-    appBarTheme: AppBarTheme(
-      backgroundColor: Colors.pink
-    )
+    useMaterial3: true
   );
 }
